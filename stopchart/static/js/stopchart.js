9a13059ea5b06a163bab5c7b0a08708ff4817574
cerickson <basicsafty@gmail.com>
2016-09-27 23:20:35 -0400

    added onclick to change colors, refactored into update function

--- stopchart/static/js/stopchart.js
+++ stopchart/static/js/stopchart.js
@@ -11,19 +11,28 @@ function extractData(data) {
   return flatdata;
 }
 
-function nestedIndex(data, j) {
-  newdata = []
-  for (key in data) {
-    newdata.push([j, data[key]]);
+function flatten(data) {
+  var newdata = [];
+  for (var i=0; i < data.length; i++) {
+    var nest = nestedIndex(data[i].data, i, data[i].name);
+    newdata = newdata.concat(nest);
   }
   return newdata;
 }
 
-var colorkey = {0:"green", 1:"yellow", 2:"red"};
+function nestedIndex(data, j, name) {
+  newdata = []
+  for (var i=0; i<data.length; i++) {
+    newdata.push({x: i, y: j, colorkey: data[i], name: name});
+  }
+  return newdata;
+}
+
+var colorkey = ["green", "yellow", "red"];
 
 var radius = 50;
-var rowheaders = ["good", "cool", "wow"];
-var dataset = [{name: "Boeing", data: [1, 2, 0]},
+var default_rowheaders = ["good", "cool", "wow"];
+var default_data = [{name: "Boeing", data: [1, 2, 0]},
                {name: "Airbus", data: [1, 1, 1]},
                {name: "Embraer", data: [2, 0, 0]}
               ];
@@ -52,52 +61,116 @@ svg.append("rect")
     .attr("fill", "none")
     .attr("stroke", "black")
     .attr("stroke-width", 2);
+//
+// svg.append("g")
+//     .attr("id", "svg-row-headers");
+//
+// svg.append("g")
+//     .attr("id", "svg-circles");
+//
+// svg.append("g")
+//     .attr("id", "svg-col-headers");
 
-svg.selectAll("text")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .attr("x", width / (dataset.length + 1) / 2)
-    .attr("y", function(d, i) {
-      return (20 + 10 + radius / 2) * (i + 1);
-    })
-    .text(function(d) {
-      return d.name;
-    })
-    .attr("text-anchor", "middle");
+update(default_rowheaders, default_data);
 
-svg.append("g")
-    .selectAll("text")
-    .data(rowheaders)
-    .enter()
-    .append("text")
-    .attr("x", function(d, i) {
-      return width / (dataset.length + 1) * (i + 1) + radius / 2;
-    })
-    .attr("y", function(d, i) {
-      return 20;
-    })
-    .text(function(d) {
-      return d;
-    })
-    .attr("text-anchor", "middle");
+function update(headers, rowdata) {
+  dataset = rowdata;
+  colheaders = headers;
+
+  var t = d3.transition()
+      .ease(d3.easeQuadIn)
+      .duration(500);
+
+  var svgRowHeaders = svg.selectAll(".svg-row-header")
+      .data(dataset);
+
+  svgRowHeaders.exit()
+      .transition(t)
+      .remove()
+
+  svgRowHeaders.transition(t)
+
+  svgRowHeaders.enter()
+      .append("text")
+      .attr("class", "svg-row-header")
+      .attr("x", width / (dataset.length + 1) / 2)
+      .attr("y", function(d,i) {
+        return height + radius * i;
+      })
+      .transition(t)
+      .attr("y", function(d, i) {
+        return (20 + 10 + radius / 2) * (i + 1);
+      })
+      .text(function(d) {
+        return d.name;
+      })
+      .attr("text-anchor", "middle");
+
+  var svgColHeaders = svg.selectAll(".svg-col-header")
+      .data(colheaders);
+
+  svgColHeaders.exit()
+      .transition(t)
+      .remove()
+
+  svgColHeaders.transition(t)
+
+  svgColHeaders.enter()
+      .append("text")
+      .attr("class", "svg-col-header")
+      .attr("x", function(d, i) {
+        return width / (dataset.length + 1) * (i + 1) + radius / 2;
+      })
+      .attr("y", function(d, i) {
+        return 20;
+      })
+      .text(function(d) {
+        return d;
+      })
+      .attr("text-anchor", "middle");
 
 
-svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("g")
-    .selectAll("circle")
-    .data(function(d, i) { return nestedIndex(d.data, i); })
-    .enter()
-    .append("circle")
-    .attr("r", radius / 2 )
-    .attr("cx", function(d, i) {
-      return (width / (dataset.length + 1)) * (i + 1) + radius / 2
-    })
-    .attr("cy", function(d) {
-      return (20 + 10 + radius / 2) * (d[0] + 1);
-    })
-    .attr("fill", function(d) {
-      return colorkey[d[1]]
-    });
+  var svgCircles = svg.selectAll("circle")
+      .data(flatten(dataset));
+
+  svgCircles.exit()
+      .transition(t)
+      .remove();
+
+  svgCircles.transition(t)
+      .attr("r", radius / 2 )
+      .attr("cx", function(d) {
+        return (width / (dataset.length + 1)) * (d.x + 1) + radius / 2
+      })
+      .attr("cy", function(d) {
+        return (20 + 10 + radius / 2) * (d.y + 1);
+      })
+      .attr("fill", function(d) {
+        return colorkey[d.colorkey];
+      });
+
+  svgCircles.enter()
+      .append("circle")
+      .on("click", function(d) {
+          var newcolor = d.colorkey + 1;
+          if (newcolor >= colorkey.length) {
+              newcolor = 0;
+          };
+          dataset[d.y].data[d.x] = newcolor;
+          update(colheaders, dataset);
+      })
+      .attr("r", radius / 2 )
+      .attr("cx", function(d) {
+        return (width / (dataset.length + 1)) * (d.x + 1) + radius / 2
+      })
+      .attr("cy", function(d) {
+        return height + radius * d.y;
+      })
+      .transition(t)
+      .attr("cy", function(d) {
+        return (20 + 10 + radius / 2) * (d.y + 1);
+      })
+      .attr("fill", function(d) {
+        return colorkey[d.colorkey];
+      });
+}
